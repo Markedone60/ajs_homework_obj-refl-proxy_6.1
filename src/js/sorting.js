7372f04270a13c97188db7ef3141835c137a4fe9
Grigory <gregory.rvl@yandex.ru>
2022-09-17 21:38:57 +0300

    fixing issues with build

--- src/js/sorting.js
+++ src/js/sorting.js
@@ -17,17 +17,3 @@ export default function orderByProps(obj, sorting) {
 
   return initialArr.concat(sortedArr);
 }
-
-/*
-
-[
-  {key: "name", value: "мечник"}, // порядок взят из массива с ключами
-  {key: "level", value: 2}, // порядок взят из массива с ключами
-  {key: "attack", value: 80}, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
-  {key: "defence", value: 40}, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
-  {key: "health", value: 10} // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
-]
-
-// ["name", "level"]
-
-*/
